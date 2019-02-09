d138508ed2f151bc31d9645ec0d8d9fe9c6adaf5
MAJID NISAR <majidnisar@gmail.com>
2019-02-09 12:29:39 +0530

    minor fix in styling

--- routes/index.js
+++ routes/index.js
@@ -1,34 +1,37 @@
-var express = require("express");
-var router = express.Router();
-let Handlebars = require("hbs");
+var express = require('express')
+var router = express.Router()
+let Handlebars = require('hbs')
+
+// creating a context object for the view
+let context = {
+  people: [
+    { firstName: 'Yehuda', lastName: 'Katz' },
+    { firstName: 'Carl', lastName: 'Lerche' },
+    { firstName: 'Alan', lastName: 'Johnson' }
+  ],
+  title: 'Index Page',
+  condition: false,
+  object: {
+    objectItem1: 'value item1',
+    onjectItem2: 'value item2 '
+  },
+  anyArray: [1, 2, 3],
+  user: 'Majid Nisar'
+}
 
 // customer helper using handle bars
-Handlebars.registerHelper("list", function(items, options) {
-  var out = "<ul>";
+Handlebars.registerHelper('list', function (items, options) {
+  var out = '<ul>';
 
   for (var i = 0, l = items.length; i < l; i++) {
-    out = out + "<li>" + options.fn(items[i]) + "</li>";
+    out = out + '<li>' + options.fn(items[i]) + '</li>';
   }
 
-  return out + "</ul>";
-});
+  return out + '</ul>';
+})
 /* GET home page. */
-router.get("/", function(req, res, next) {
-  res.render("index", {
-    people: [
-      { firstName: "Yehuda", lastName: "Katz" },
-      { firstName: "Carl", lastName: "Lerche" },
-      { firstName: "Alan", lastName: "Johnson" }
-    ],
-    title: "Index Page",
-    condition: false,
-    object: {
-      objectItem1: "value item1",
-      onjectItem2: "value item2 "
-    },
-    anyArray: [1, 2, 3],
-    user: "Majid Nisar"
-  });
-});
+router.get('/', function (req, res, next) {
+  res.render('index', context)
+})
 
-module.exports = router;
+module.exports = router
